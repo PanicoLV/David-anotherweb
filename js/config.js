30fe78149cd53fae05c53b316841bda8f8d8aa9e
PanicoLV <98421273+PanicoLV@users.noreply.github.com>
2022-11-28 19:13:21 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-east-2_pLGHDs0oj ',         // e.g. us-east-2_pLGHDs0oj
-        userPoolClientId:   '4qkiq3oq15jcb5vq3f3tng76q',         // e.g. 4qkiq3oq15jcb5vq3f3tng76q
-        region:           ' us-east-2 '          // e.g. us-east-2
+        userPoolId:'us-east-2_pLGHDs0oj ',         // e.g. us-east-2_pLGHDs0oj
+        userPoolClientId:'4qkiq3oq15jcb5vq3f3tng76q',         // e.g. 4qkiq3oq15jcb5vq3f3tng76q
+        region:' us-east-2 '          // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
