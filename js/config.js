631bcd5ab52e43fb9211f1c2bbbe8d5569c2d5b6
PanicoLV <98421273+PanicoLV@users.noreply.github.com>
2022-11-29 04:00:18 -0600

    Update config.js
    change invoke url

--- js/config.js
+++ js/config.js
@@ -8,3 +8,4 @@ window._config = {
         invokeUrl: 'https://kj39v96vr9.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+   
